f181c2555033db458c50574dedd7e33a18038ad3
Elena <Elena3405674@mail.ru>
2024-07-14 19:58:53 +0300

    Updates

---
+++ static/js/616.70763255.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhooks=self.webpackChunkhooks||[]).push([[616],{616:(e,s,h)=>{h.r(s),h.d(s,{default:()=>k});var u=h(579);function k(){return(0,u.jsx)("h2",{children:"Home"})}}}]);
+//# sourceMappingURL=616.70763255.chunk.js.map
